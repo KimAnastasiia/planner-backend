4d3114e153e66feaf0702e28e4ccaff76514f027
Anastasiia <anstasiakim2789@gmail.com>
2024-02-15 15:36:45 +0100

    get all invitations of user

--- src/invited/controller/invited.controller.ts
+++ src/invited/controller/invited.controller.ts
@@ -1,4 +1,24 @@
-import { Controller } from '@nestjs/common';
+/* eslint-disable prettier/prettier */
+import { Controller, Req, Get, HttpStatus, HttpException } from '@nestjs/common';
+import { InvitedService } from '../services/invited.service';
 
 @Controller('invited')
-export class InvitedController {}
+export class InvitedController {
+
+    constructor(private readonly invitedService: InvitedService) { }
+
+    @Get()
+    async getMeeting(@Req() request: Request) {
+        const email = request["userEmail"]
+        try {
+        const invitations = await this.invitedService.getInvitations(email);
+        return invitations;
+        } catch (error) {
+        console.error(error);
+        throw new HttpException({
+            success: false,
+            error: 'Failed to get invitations.',
+        }, HttpStatus.BAD_REQUEST);
+        }
+    }
+}
--- src/invited/services/invited.service.ts
+++ src/invited/services/invited.service.ts
@@ -1,4 +1,25 @@
+/* eslint-disable prettier/prettier */
 import { Injectable } from '@nestjs/common';
-
+import { InjectRepository } from '@nestjs/typeorm';
+import { invited } from '../typeorm/Invited.entity';
+import { Repository } from 'typeorm'
 @Injectable()
-export class InvitedService {}
+
+export class InvitedService {
+
+  constructor(
+      @InjectRepository(invited)
+      private invitationsRepository: Repository<invited>,
+    ) { }
+  
+    async getInvitations(email:string): Promise<invited[]> {
+
+      try{
+          const invitations = await this.invitationsRepository.find({ where: { email }, relations: ['meeting'], });
+          return invitations
+      }catch(err){
+          throw new Error('Failed to get invitations.');
+        }
+    
+    }
+}
